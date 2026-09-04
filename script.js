339106b4b7992d1ec6905c8da2bdbe715b13e88c
shs2211205baloyo-cpu <shs2211205.baloyo@csav.edu.ph>
2026-09-04 16:33:11 +0800

    querySelector

--- script.js
+++ script.js
@@ -20,4 +20,13 @@ function greet(greetings, names = []) {
 }
 console.log(greet("Mayung aga", ["Mario", "Carl", "Bobet", "Kas", "Mico"]));
 
+const heading = document.querySelector("h1");
+console.log(heading);
+
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading);
+
+const projectHeading = document.querySelector("#services h2");
+console.log(projectHeading);
+
 
