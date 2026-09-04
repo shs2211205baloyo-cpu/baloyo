cb86b767af5c6f5c9e043d9b1a26e101266f4fb6
shs2211205baloyo-cpu <shs2211205.baloyo@csav.edu.ph>
2026-09-04 17:29:10 +0800

    addEventListener

--- script.js
+++ script.js
@@ -28,11 +28,30 @@ heading.style.backgroundColor = "Yellow";
 heading.style.fontSize = "100px";
 console.log(heading);
 
+heading.addEventListener("click", function () {
+    heading.style.color = "red";
+    console.log("Heading was clicked.")
+});
+
 const contactHeading = document.querySelector("#contact h2");
 console.log(contactHeading);
 
 const projectHeading = document.querySelector("#services h2");
 console.log(projectHeading);
 
+const toggleButton = document.querySelector('#switch')
+const body = document.querySelector('body');
+let isOff = false;
 
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white"
+        body.style.color = "black";
+    }
+})
 
