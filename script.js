a7ed0f2b15f1c21f63e1248bde9f758f47ff5924
shs2211205baloyo-cpu <shs2211205.baloyo@csav.edu.ph>
2026-09-04 14:37:42 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Hellow World.");
 console.log("From script.js file.");
+
+const myName = "Mario";
+let age = 20;
+const myNumber = "09944150401";
+const myAddress = "hda. 1";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Address: ${myAddress}`);
+console.log(67);
+
+//console.log(`My name is ${myName}, ${age} years old.`)
+
